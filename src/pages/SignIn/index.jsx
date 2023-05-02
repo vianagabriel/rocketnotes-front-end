b652b3c7331e53ea6f935b9fdbfba6718b3b1866
Gabriel Viana <gabrielviana@MacBook-Pro-de-Gabriel.local>
2023-05-02 00:23:45 -0300

    lógica da pagina signIn e signUp feitas, criação de sessão feita

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import { Container, Form, Background } from './styles';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
 import { FiMail, FiLock } from 'react-icons/fi';
 
+import { useAuth } from '../../hooks/auth';
+
 import { Link } from 'react-router-dom';
 
 
+
 export function SignIn() {
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const { signIn } = useAuth();
+
+    function handleSignIn(){
+        signIn({ email, password })
+    }
+
+
     return (
         <Container>
             <Form>
@@ -20,6 +34,7 @@ export function SignIn() {
                     placeholder='E-mail'
                     type='text'
                     icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
                 />
 
 
@@ -27,9 +42,10 @@ export function SignIn() {
                     placeholder='Senha'
                     type='password'
                     icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
                 />
 
-                <Button title='Entrar'/>
+                <Button title='Entrar' onClick={handleSignIn} />
 
                 <Link to='/register'>Criar conta</Link>
 
