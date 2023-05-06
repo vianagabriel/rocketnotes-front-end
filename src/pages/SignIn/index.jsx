bf62a69d8f75eb213fd050e4b8efb67b62af77fb
Gabriel Viana <gabrielviana@MacBook-Pro-de-Gabriel.local>
2023-05-06 15:32:10 -0300

    subindo lógica de usuário permanecer logado na aplicação através do local storage

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
+import { useAuth } from '../../hooks/auth';
 import { Container, Form, Background } from './styles';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
 import { FiMail, FiLock } from 'react-icons/fi';
 
-import { useAuth } from '../../hooks/auth';
 
 import { Link } from 'react-router-dom';
 
